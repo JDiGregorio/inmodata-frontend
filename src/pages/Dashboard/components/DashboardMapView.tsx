7c8f4e7a57fb61ea5eb9c6907a55ea1a55c7c888
José Di Gregorio <josedgregorio07@gmail.com>
2026-02-17 11:45:41 -0600

    changes

--- src/pages/Dashboard/components/DashboardMapView.tsx
+++ src/pages/Dashboard/components/DashboardMapView.tsx
@@ -5,21 +5,21 @@ import { Dialog, DialogBackdrop, DialogPanel } from '@headlessui/react'
 import { MapCanvas } from './DashboardMapView/MapCanvas'
 
 export default function DashboardMapView() {
-  const [expanded, setExpanded] = React.useState(false)
+    const [expanded, setExpanded] = React.useState<boolean>(false)
 
-  return (
-    <>
-      <MapCanvas expanded={false} onToggleExpand={() => setExpanded(true)} />
+    return (
+        <>
+            <MapCanvas expanded={false} onToggleExpand={() => setExpanded(true)} />
 
-      <Dialog open={expanded} onClose={setExpanded} className="relative z-50">
-        <DialogBackdrop className="fixed inset-0 bg-black/90" />
+            <Dialog open={expanded} onClose={setExpanded} className="relative z-50">
+                <DialogBackdrop className="fixed inset-0 bg-black/90" />
 
-        <div className="fixed inset-0 p-3 sm:p-6">
-          <DialogPanel className="relative h-full w-full overflow-hidden rounded-2xl bg-white shadow-2xl">
-            <MapCanvas expanded onToggleExpand={() => setExpanded(false)} />
-          </DialogPanel>
-        </div>
-      </Dialog>
-    </>
-  )
+                <div className="fixed inset-0 p-3 sm:p-6">
+                    <DialogPanel className="relative h-full w-full overflow-hidden rounded-2xl bg-white shadow-2xl">
+                        <MapCanvas expanded onToggleExpand={() => setExpanded(false)} />
+                    </DialogPanel>
+                </div>
+            </Dialog>
+        </>
+    )
 }
--- src/pages/Dashboard/components/DashboardMapView/types.ts
+++ src/pages/Dashboard/components/DashboardMapView/types.ts
@@ -1,13 +1,13 @@
 export type Point = {
-  id: string
-  name: string
-  position: google.maps.LatLngLiteral
-  address?: string
-  notes?: string
+	id: string;
+	name: string;
+	position: google.maps.LatLngLiteral;
+	address?: string;
+	notes?: string;
 }
 
 export type SelectedPlace = {
-  position: google.maps.LatLngLiteral
-  name?: string
-  address?: string
+	position: google.maps.LatLngLiteral;
+	name?: string;
+	address?: string;
 }
